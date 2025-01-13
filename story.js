00dcc91e5e1f1e036be5f875e53719b280362395
fonsii <abeaalfonso@gmail.com>
2025-01-14 01:17:56 +0400

    Js

--- story.js
+++ story.js
@@ -3,3 +3,14 @@ function toggle() {
     const menu = document.getElementById('nav-menu');
     menu.classList.toggle('open');
 }
+
+//the content of the story
+const TheStory = [
+    {
+        content: ( <div class="cover">
+        <h1>Ponder</h1>
+        <p class="sub">A Fairytale Story</p>
+        </div>
+        ),
+    },
+]
