12a4c0b9778a6211f63628117f00464b871ddf14
fonsii <abeaalfonso@gmail.com>
2025-01-14 11:32:02 +0400

    commit

---
+++ story.js
@@ -0,0 +1,16 @@
+// function fot toggling the navigation bar
+function toggleMenu() {
+    const menu = document.getElementById('nav-menu');
+    menu.classList.toggle('open');
+}
+
+//the content of the story
+const TheStory = [
+    {
+        content: ( <div class="cover">
+        <h1>Ponder</h1>
+        <p class="sub">A Fairytale Story</p>
+        </div>
+        ),
+    },
+]
