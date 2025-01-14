65ab929f6fa562757f5e90f2cf74c210c2bf370e
fonsii <abeaalfonso@gmail.com>
2025-01-14 13:59:36 +0400

    commit

--- backend/routes/userData.js
+++ backend/routes/userData.js
@@ -4,7 +4,7 @@ import User from '../models/userModel.js';
 const router = express.Router();
 
 router.post ('/', async (req, res) => {
-    const { name, email, password } = req.body;
+    const { email, password } = req.body;
     try {
         const user = await User.create({ name, email, password });
         await user.save();
